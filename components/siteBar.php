129006bc2e0ffabb84c3b67c58a9f2fbccc25e69
Le Van Dat <72507371+lvdat@users.noreply.github.com>
2023-09-07 18:26:48 +0700

    fix link style in footer.

--- components/siteBar.php
+++ components/siteBar.php
@@ -82,8 +82,8 @@
     </div>
 </div>
 <?php endif ?>
-<div class="card credit__info text-center">
-    <div class="card-body text-muted small">
+<div class="card text-center">
+    <div class="card-body text-muted small credit__info">
         Hosted by <a href="<?=$_ENV['HOSTING_PROVIDED_LINK']?>" target="_blank"><?=$_ENV['HOSTING_PROVIDED_NAME']?></a>
         | <i class="fas fa-code-commit"></i> <?=getCurrentCommit(8)?> | <a
             href="https://github.com/lvdat/pldv-ktpm04-web" target="_blank"><i class="fab fa-github"></i> Source
